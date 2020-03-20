26a86d68a4e9940763a4c361d5aa171c343f5c2c
tis <tisbootis@gmail.com>
2020-03-20 22:03:28 +0300

    add main-swiper section

---
+++ source/js/main.js
@@ -0,0 +1,25 @@
+// [index page]
+// главный слайдер
+var mySwiper = new Swiper('#main-swiper', {
+  // effect: 'fade',
+  // fadeEffect: { crossFade: true },
+
+  autoHeight: true,
+
+  pagination: {
+    el: '.main-swiper-pagination',
+    type: 'fraction',
+    renderFraction: function (currentClass, totalClass) {
+      return '0' + 
+             '<span class="' + currentClass + '"></span>' +
+             ' / ' +
+             '0' +
+             '<span class="' + totalClass + '"></span>';
+    }
+  },
+
+  navigation: {
+    nextEl: '.main-swiper__button-next',
+    prevEl: '.main-swiper__button-prev',
+  },
+});

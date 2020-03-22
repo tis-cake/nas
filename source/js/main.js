d57c1901c789e57d346671c91b0b150ddac6e561
tis <tisbootis@gmail.com>
2020-03-22 23:29:28 +0300

    add script for swiper-pagination-fraction if slides >= 10

--- source/js/main.js
+++ source/js/main.js
@@ -1,20 +1,25 @@
 // [index page]
 // главный слайдер
+var mainSwiperArr = document.querySelectorAll('.main-swiper__slide');
 var mainSwiper = new Swiper('#main-swiper', {
   // effect: 'fade',
   // fadeEffect: { crossFade: true },
-
-  // autoHeight: true,
-
   pagination: {
     el: '.main-swiper__pagination',
     type: 'fraction',
     renderFraction: function (currentClass, totalClass) {
-      return '0' + 
+      if (mainSwiperArr.length >= 10) {
+        return '<span class="swiper-pagination-current-zero">0</span>' + 
+             '<span class="' + currentClass + '"></span>' +
+             ' / ' +
+             '<span class="' + totalClass + '"></span>';
+      } else {
+      return '<span class="swiper-pagination-current-zero">0</span>' + 
              '<span class="' + currentClass + '"></span>' +
              ' / ' +
              '0' +
              '<span class="' + totalClass + '"></span>';
+      }
     }
   },
 
@@ -24,11 +29,15 @@ var mainSwiper = new Swiper('#main-swiper', {
   },
 });
 
+if (mainSwiperArr.length >= 10) {
+  mainSwiper.on('slideChange', function () {
+    updateFraction(this);
+  });
+}
+
 // слайдер проектов
 var projectsSwiper = new Swiper('#projects-swiper', {
   slidesPerView: 1,
-  // autoHeight: true,
-  // spaceBetween: 100,
 
   navigation: {
     nextEl: '.projects-swiper__button-next',
@@ -62,6 +71,7 @@ var twoArticlesSwiper = new Swiper('#two-articles-swiper', {
 });
 
 // слайдер отзывов
+var reviewsSwiperArr = document.querySelectorAll('.reviews-swiper__slide');
 var reviewsSwiper = new Swiper('#reviews-swiper', {
   slidesPerView: 1,
 
@@ -69,23 +79,33 @@ var reviewsSwiper = new Swiper('#reviews-swiper', {
     el: '.reviews-swiper__pagination',
     type: 'fraction',
     renderFraction: function (currentClass, totalClass) {
-      var reviewsSwiperArr = document.querySelectorAll('.reviews-swiper__slide');
-      if (reviewsSwiperArr.length > 10) {
-        // console.log('больше');
+
+      // если слайдов >= 10 - рендерим пагинацию без "0" в totalClass
+      //  иначе - рендерим с "0"
+      if (reviewsSwiperArr.length >= 10) {
         return '<span class="swiper-pagination-current-zero">0</span>' + 
              '<span class="' + currentClass + '"></span>' +
              ' / ' +
              '<span class="' + totalClass + '"></span>';
       } else {
-
       return '<span class="swiper-pagination-current-zero">0</span>' + 
              '<span class="' + currentClass + '"></span>' +
              ' / ' +
              '0' +
              '<span class="' + totalClass + '"></span>';
-           }
+      }
     }
   },
+
+  // при изменении слайда - проверяем, является ли текущий слайд 10+ по счёту,
+  //  чтобы убрать/добавить ноль к пагинации 
+  // on: {
+  //   slideChange: function () {
+  //     if (reviewsSwiperArr.length >= 10) {
+  //       updateFraction(this);
+  //     }
+  //   },
+  // },
 
   navigation: {
     nextEl: '.reviews-swiper__button-next',
@@ -100,34 +120,24 @@ var reviewsSwiper = new Swiper('#reviews-swiper', {
   // }
 });
 
-reviewsSwiper.on('slideChange', function () {
-  // console.log('slide changed');
-  console.log(reviewsSwiper.activeIndex+1);
-  if (this.activeIndex+1 >= 10) {
-    // console.log('bleaaa');
-    // renderFraction() {
-      // return console.log('ssss');
-    // }
+// работа с нулём в пагинации
+//  на этот раз - функция запускается только если слайдов >= 10
+if (reviewsSwiperArr.length >= 10) {
+  reviewsSwiper.on('slideChange', function () {
+    updateFraction(this);
+  });
+}
+
+// индекс активного слайда >= 10 - скрыть "0", иначе если < 10 - показать
+function updateFraction(slider) {
+  
+  var sliderActiveIndex = slider.activeIndex + 1;
+  var sliderElement = slider.el;
+  var fractionActiveZero = sliderElement.querySelector('.swiper-pagination-current-zero');
+
+  if (sliderActiveIndex >= 10) {
+    fractionActiveZero.classList.add('hidden');
+  } else if (sliderActiveIndex < 10) {
+    fractionActiveZero.classList.remove('hidden');
   }
-});
-
-// function updateFraction (reviewsSwiper) {
-//   console.log(this.activeIndex+1);
-//   if (this.activeIndex+1 >= 10) {
-//     console.log('bleaaa');
-//   }
-// }
-
-// console.log(reviewsSwiper.activeIndex+1);
-// var reviewsCurrent = reviewsSwiper.activeIndex+1;
-// if (reviewsCurrent >= 10) {
-//   console.log('bleaaa');
-// }
-
-// console.log(reviewsCurrent.length);
-
-// var reviewsSwiperArr = document.querySelectorAll('.reviews-swiper__slide');
-// console.log(reviewsSwiperArr.length);
-// if (reviewsSwiperArr.length > 10) {
-//   console.log('больше');
-// }
+}

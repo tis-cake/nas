bb485a1cf8149570e45036463446e0901acda717
tis <tisbootis@gmail.com>
2020-03-29 20:30:17 +0300

    add js for slideUp/slideDown on about page (without jq)

--- source/js/main.js
+++ source/js/main.js
@@ -259,3 +259,77 @@ let memberSwiper = new Swiper('#team-swiper-member', {
   //   }
   // }
 });
+
+// slideUp/slideDown
+let chronicleBtnArr = document.querySelectorAll('.chronicle__btn');
+for (let chronicleBtn of chronicleBtnArr) {
+  chronicleBtn.addEventListener('click', function (evt) {
+    let chronicleBlock = this.closest('.chronicle__block');
+    let chronicleList = chronicleBlock.querySelector('.chronicle__list');
+
+    // если список пуст - выйти из функции
+    if (chronicleList.children.length === 0) {
+      return;      
+    }
+
+    this.classList.toggle('active');
+    chronicleList.classList.toggle('active');
+
+    // второй аргумент - время на исполнение setInterval
+    if (chronicleList.classList.contains('active')) {
+      slideUp(chronicleList, 15);
+    } else {
+      slideDown(chronicleList, 15);
+    }
+
+  });
+}
+
+  // let height = 0;
+  // let interval = 0;
+  // let counter = 0;
+
+// скрыть элементы (элемент, время)
+function slideUp(item, duration) {
+  
+  height = item.offsetHeight;
+  counter = height;
+
+  let subtractor = height/10;
+
+  // сейчас это в css
+  // item.style.overflow = "hidden";
+  // item.style.cssText = "transition: height 500ms; -webkit-transition: height 500ms; -moz-transition: height 500ms; -o-transition: height 500ms";
+
+  interval = setInterval(function () {
+    
+    // выполняем, пока счётчик не обнулится
+    // высота элемента на каждой итерации приравнивается к значению счётчика
+    counter -= subtractor;
+    // console.log(counter); 
+    if (counter > 0) {
+      item.style.height = counter + "px";
+    } else {
+      item.style.height = 0;    // счётчик обнулился - высота = 0      
+      clearInterval(interval);  // прекратить выполнение
+    }
+  }, duration);                 // выполнять в течение времени (переданный второй аргумент)
+}
+
+// показать элементы (элемент, время)
+function slideDown(item, duration) {
+  let adder = height/10;        
+
+  // item.style.cssText = "transition: none; -webkit-transition: none; -moz-transition: none; -o-transition: none";
+  
+  interval = setInterval(function () {            
+    counter += adder;
+    // console.log(counter);
+    if (counter < height) {
+      item.style.height = counter + "px";
+    } else {
+      item.style.height = height + "px";
+      clearInterval(interval);
+    }
+  }, duration); 
+}
